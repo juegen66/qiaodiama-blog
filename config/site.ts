90fa3e78ff6148107eba1910ba48b89e0b69040a
goodstudyp <140302724+goodstudyp@users.noreply.github.com>
2025-08-10 16:21:36 +0800

    占时

--- config/site.ts
+++ config/site.ts
@@ -9,8 +9,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Life",
+      href: "/life",
     },
     {
       label: "Pricing",
